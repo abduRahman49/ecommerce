e9209ec28761f160bd0c6165a3fd24e55c693a83
abdouseye <abdou.seye@gsietechnology.com>
2024-07-24 11:05:06 +0000

    Ajout views register et login

--- authentication/views.py
+++ authentication/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCreationForm(request.POST) # Ce formulaire permet de créer un nouvel utilisateur
         if form.is_valid():
             form.save() # A ce niveau le hashage est géré automatiquement par Django. La méthode save de UserCreationForm va faire appel à une autre méthode set_password qui hashera le mot de passe avant de le sauvegarder
             username = form.cleaned_data.get('username')
@@ -21,7 +21,7 @@ def signup(request):
 
 def signin(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = AuthenticationForm(request, data=request.POST) # Le formulaire utilisé est utilisé spécifiquement pour l'authentification (Login)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
