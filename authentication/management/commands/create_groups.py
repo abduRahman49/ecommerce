4ea1e9924db433c50b6283342d2d4956ad1e6979
abduRahman49 <sabdouseye@gmail.com>
2024-07-26 10:59:46 +0000

    Ajout de commentaire sur les commandes

--- authentication/management/commands/create_groups.py
+++ authentication/management/commands/create_groups.py
@@ -6,15 +6,18 @@ class Command(BaseCommand):
     help = "Permet de créer un groupe d'utilisateurs"
 
     def add_arguments(self, parser):
+        # Permet de récupérer les arguments de la commande et les stocker dans une liste qui aura pour nom le premier argument de parser.add_argument()
         parser.add_argument("group_names", nargs="+", type=str)
 
     def handle(self, *args, **options):
+        # Permet d'effectuer les opérations que l'on souhaite automatiser et définir comme commande d'administration
         group_names = options["group_names"]
         for group_name in group_names:
+            # Récupère ou crée le groupe (groupe de permissions auquel des utilisateurs peuvent être ajoutés)
             _, created = Group.objects.get_or_create(
                 name=group_name
             )
-            if not created:
+            if not created:  # Si le groupe n'est pas créé alors il existe déja et la variable created sera évaluée à False
                 raise CommandError(f'Le groupe avec le nom {group_name} existe déjà')
 
             self.stdout.write(
